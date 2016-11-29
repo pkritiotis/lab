5871a7a7684b627ef624ae140f1d2cdf406d36ac
Panayiotis Kritiotis <pkritiotis@gmail.com>
2016-11-29 20:31:24 +0200

    Added a c# sample program which uses a custom configuration in App.config

---
+++ small_experiments/CustomConfigurationSection/CustomConfigurationSection/MyApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConfigurationSection
+{
+    public class MyApp
+    {
+
+        public static void Main(string[] args)
+        {
+            MyAppSpecificConfigurationSection Configs = (MyAppSpecificConfigurationSection)System.Configuration.ConfigurationManager.GetSection("myAppSpecificConfigurationSection");
+
+            Console.WriteLine(Configs.Active);
+            Console.WriteLine(Configs.ApplicationDetails.Name);
+            Console.WriteLine(Configs.ApplicationDetails.Version);
+        }
+    }
+}
---
+++ small_experiments/CustomConfigurationSection/CustomConfigurationSection/MyAppCustomConfigurationSection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConfigurationSection
+{
+    public class MyAppSpecificConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationProperty("active", DefaultValue = "true", IsRequired = false)]
+        public Boolean Active
+        {
+            get
+            {
+                return (Boolean)this["active"];
+            }
+            set
+            {
+                this["active"] = value;
+            }
+        }
+
+        [ConfigurationProperty("application")]
+        public ApplicationDetails ApplicationDetails
+        {
+            get
+            {
+                return (ApplicationDetails)(this["application"]);
+            }
+            set
+            { this["application"] = value; }
+        }
+        
+    }
+
+    public class ApplicationDetails : ConfigurationElement
+    {
+        [ConfigurationProperty("name",  IsRequired = true)]
+        public String Name
+        {
+            get
+            {
+                return (String)this["name"];
+            }
+            set
+            {
+                this["name"] = value;
+            }
+        }
+
+        [ConfigurationProperty("version",  IsRequired = true, DefaultValue = "0.0.0")]
+        [RegexStringValidator(@"^\d\.\d\.\d$")]
+        public string Version
+        {
+            get
+            { return Convert.ToString(this["version"]); }
+            set
+            { this["version"] = value; }
+        }
+    }
+}
